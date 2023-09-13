a12e7b95699fc000422bab07129b138f578eeeb4
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-09-13 16:58:04 -0400

    Dashboard Table Cleanup

--- client/src/view/Dashboard.jsx
+++ client/src/view/Dashboard.jsx
@@ -2,9 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const Dashboard = () => {
-	const [authorList, setAuthorList] = useState([]);
+    const [authorList, setAuthorList] = useState([]);
 
     useEffect(() => {
         axios
@@ -13,58 +12,56 @@ const Dashboard = () => {
             .catch((err) => console.log(err));
     }, []);
 
-	const handleDelete = (deleteId) => {
-		axios.delete(`http://localhost:8000/api/authors/${deleteId}`)
-		.then(()=>{
-			removeFromDom(deleteId);
-			console.log("deleted!")
+    const handleDelete = (deleteId) => {
+        axios
+            .delete(`http://localhost:8000/api/authors/${deleteId}`)
+            .then(() => {
+                removeFromDom(deleteId);
+                console.log("deleted!");
+            })
+            .catch((err) => console.log(err));
+    };
 
-		})
-		.catch(err=>console.log(err));
-	}
+    const removeFromDom = (deletedId) => {
+        const filterdList = authorList.filter(
+            (eachAuthor, idx) => eachAuthor._id !== deletedId
+        );
+        setAuthorList(filterdList);
+    };
 
-	const removeFromDom = (deletedId) => {
-		const filterdList = authorList.filter(
-			(eachAuthor, idx) => eachAuthor._id !== deletedId
-		);
-		setAuthorList(filterdList)
-	}
+    return (
+        <>
+            <Link to={`/authors/new`}>Add an author</Link>
+            <p>We have quotes by:</p>
+            <table>
+                <tr>
+                    <th>Author</th>
+                    <th>Actions Available</th>
+                </tr>
+                {authorList.map((oneAuthor, idx) => {
+                    return (
+                        <tr key={idx}>
+                            <td>{oneAuthor.name}</td>
+                            <td>
+                                <Link to={`/authors/${oneAuthor._id}/edit`}>
+                                    <button>Edit</button>
+                                </Link>
+                                <Link to={`/authors/`}>
+                                    <button
+                                        onClick={() =>
+                                            handleDelete(oneAuthor._id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
+                                </Link>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </table>
+        </>
+    );
+};
 
-  return (
-	  <>
-	  
-		<Link to={`/authors/new`}>Add an author</Link> 
-		<p>We have quotes by:</p>
-	<div> {authorList.map((oneAuthor, idx) => {
-		return (
-			<div key={idx}>
-				<table>
-					<tr>
-						<th>Author</th>
-						<th>Actions Available</th>
-					</tr>
-					<tr>
-						<td>{oneAuthor.name}</td>
-						<td>
-							<Link to={`/authors/${oneAuthor._id}/edit`}>
-							<button>Edit</button> 
-							</Link>
-							<Link to={`/authors/`}>
-							<button onClick={() => handleDelete(oneAuthor._id)}>
-								Delete
-							</button> 
-							</Link>
-							</td>
-					</tr>
-					
-				</table>
-				
-				
-			</div>
-		);
-	})}</div>
-			</>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
